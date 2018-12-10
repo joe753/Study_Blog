9e94e3ca18301d127103454feab04b1f6b4e11a9
joe753 <44761588+joe753@users.noreply.github.com>
2018-12-10 16:39:37 +0900

    Update MYSQL(2).sql

--- MYSQL(2).sql
+++ MYSQL(2).sql
@@ -1,3 +1,4 @@
+start transaction;
 
 insert into Enroll(student, subject)
  select id, (select id from Subject order by rand() limit 1) sub_id from Student order by id ;
@@ -13,3 +14,5 @@ insert into Enroll(student, subject)
 select en.*, s.name as 'student name', sub.name as 'subject name' 
 from Enroll as en inner join Student as s on en.student = s.id 
 				  inner join Subject as sub on en.subject = sub.id order by s.name;
+
+commit;
